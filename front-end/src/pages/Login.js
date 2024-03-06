ddb963957719a7dedcd785959d7231b6da6b8a8a
Misha Seo <seo.misha.lee@gmail.com>
2024-03-05 22:50:32 -0500

    first commit

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import "./Login.css";
+import Header from "../Components/Header";
+import { useNavigate } from "react-router-dom";
+
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+} from "mdb-react-ui-kit";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    console.log(data);
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <MDBContainer fluid>
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard
+              className="bg-dark text-white my-5 mx-auto"
+              style={{ borderRadius: "1rem", maxWidth: "400px" }}
+            >
+              <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                <p className="text-white-50 mb-5">
+                  Please enter your login and password!
+                </p>
+
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-white"
+                  label="Email address"
+                  id="formControlLg"
+                  type="email"
+                  size="lg"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-white"
+                  label="Password"
+                  id="formControlLg"
+                  type="password"
+                  size="lg"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+
+                <MDBBtn
+                  outline
+                  className="mx-2 px-5"
+                  color="white"
+                  size="lg"
+                  onClick={onSubmit}
+                >
+                  Login
+                </MDBBtn>
+
+                <div>
+                  <p className="mb-0">
+                    Don't have an account?{" "}
+                    <a href="#" class="text-white-50 fw-bold">
+                      Sign Up
+                    </a>
+                  </p>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ front-end/src/pages/home_page.js
@@ -0,0 +1,78 @@
+import { Form, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import "./home_page.css";
+import Header from "../Components/Header";
+
+const Home = () => {
+    //------------------- SEARCH------------------
+    const [search, setSearch] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const searchQuery = { info: search };
+        console.log(searchQuery);
+    };
+    //-----------------IMAGE SLIDER------------------
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const images = [
+        "https://res.cloudinary.com/mishaprojectcloud/image/upload/v1696242110/BARBER/img0_qshvwg.png",
+        "https://res.cloudinary.com/mishaprojectcloud/image/upload/v1696242106/BARBER/img1_u0r3rb.jpg",
+        "https://res.cloudinary.com/mishaprojectcloud/image/upload/v1696242987/BARBER/img3_nlerpp.jpg",
+        "https://res.cloudinary.com/mishaprojectcloud/image/upload/v1696243378/BARBER/img4_xnekkp.jpg",
+    ];
+    const changeImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(changeImage, 6000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    //-------------------LINE--------------------
+    const str = "FIND THE BARBER FIT FOR A BOSS\n";
+
+    return (
+        <div className="total">
+            <Header />
+            <div
+                className="background-image"
+                style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+            >
+                {/* ------------------SEARCHING---------------------- */}
+
+                <div id="searchbox">
+                    <Form
+                        onSubmit={handleSubmit}
+                        id="search"
+                        className="d-flex"
+                    >
+                        <Form.Control
+                            id="search_title"
+                            type="search"
+                            placeholder="Search for a barber"
+                            className="me-2"
+                            value={search}
+                            aria-label="Search"
+                            onChange={(event) => setSearch(event.target.value)}
+                        />
+
+                        <Button
+                            onClick={handleSubmit}
+                            id="searchButton"
+                            variant="dark"
+                        >
+                            Search
+                        </Button>
+                    </Form>
+                </div>
+                {/* --------------------SAVED BARBERS-------------------- */}
+                <div id="favBarbers"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
